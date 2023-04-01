2a273076e67abceef7837d0efc138a3373b8448a
Andreas Olsson <olsson.andreas@gmail.com>
2023-04-01 15:03:33 +0200

    part4

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ typedef enum {
 } ExecuteResult;
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE+1];
+    char email[COLUMN_EMAIL_SIZE+1];
 } Row;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
@@ -78,8 +78,10 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG
 } PrepareResult;
 
 typedef enum {
@@ -100,18 +102,40 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(input_buffer->buffer, 
-                "insert %d %s %s",
-                &(statement->row_to_insert.id),
-                statement->row_to_insert.username,
-                statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
         statement->type = STATEMENT_SELECT;
@@ -208,9 +232,9 @@ int main(int argc, char* argv[]) {
 
         if (input_buffer->buffer[0] == '.') {
             switch (do_meta_command(input_buffer)) {
-                case (META_COMMAND_SUCCESS):
+                case META_COMMAND_SUCCESS:
                     continue;
-                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
                     printf("Unrecognized command '%s'\n", input_buffer->buffer);
                     continue;
             }
@@ -218,21 +242,27 @@ int main(int argc, char* argv[]) {
 
         Statement statement;
         switch (prepare_statement(input_buffer, &statement)) {
-            case (PREPARE_SUCCESS):
+            case PREPARE_SUCCESS:
                 break;
-            case (PREPARE_SYNTAX_ERROR):
+            case PREPARE_STRING_TOO_LONG:
+                printf("String is too long.\n");
+                continue;
+            case PREPARE_NEGATIVE_ID:
+                printf("ID must be positive.\n");
+                continue;
+            case PREPARE_SYNTAX_ERROR:
                 printf("Syntax error. Could not parse statement.\n");
                 continue;
-            case (PREPARE_UNRECOGNIZED_STATEMENT):
+            case PREPARE_UNRECOGNIZED_STATEMENT:
                 printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
                 continue;
         }
 
         switch (execute_statement(&statement, table)) {
-            case (EXECUTE_SUCCESS):
+            case EXECUTE_SUCCESS:
                 printf("Executed.\n");
                 break;
-            case (EXECUTE_TABLE_FULL):
+            case EXECUTE_TABLE_FULL:
                 printf("Error: Table full.\n");
                 break;
         }
